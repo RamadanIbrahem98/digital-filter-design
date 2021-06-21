a88362b718532457f7ca7fee50d3799ee3867e98
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-06-21 21:43:17 +0200

    chore: add mag and phase calculations

--- scripts/polesAndZeros.js
+++ scripts/polesAndZeros.js
@@ -1,18 +1,27 @@
 const poles = [];
 const zeros = [];
+
 const CircleNumbersX = [];
 const CircleNumbersY = [];
+
 const PolesX = [];
 const PolesY = [];
-const Distances = [];
+const TheX_Axis = [];
+let Distances = [];
+let phases = [];
+const Distances2 = [];
+const phases2 = [];
+let Distances3 = [];
+let phases3 = [];
+
+const MuliOfDistances = new Array(315);
+const MuliOfPhases = new Array(315);
+const DivsionOfDistances = new Array(315);
+const MuliOfPhasesOfZeros = new Array(315);
+
 let blankSpace = true;
 let xaxis;
 let yaxis;
-
-for (let i = 0; i <= 3.14516; i = i + 0.01) {
-    CircleNumbersX.push(Math.cos(i));
-    CircleNumbersY.push(Math.sin(i));
-}
 
 function setup() {
     let cvs = createCanvas(600, 600);
@@ -20,6 +29,12 @@ function setup() {
     cvs.parent("canvas");
     xaxis = width / 2;
     yaxis = height / 2;
+
+    for (let i = 0; i <= 3.14516; i = i + 0.01) {
+        CircleNumbersX.push(Math.cos(i));
+        CircleNumbersY.push(Math.sin(i));
+        TheX_Axis.push(i);
+    }
 }
 
 function mousePressed() {
@@ -46,10 +61,6 @@ function mousePressed() {
         }
     }
     blankSpace = true;
-    console.log({
-        "x: ": poles[poles.length - 1].x,
-        "y: ": poles[poles.length - 1].y
-    });
     Getter();
 }
 
@@ -60,40 +71,98 @@ function Mapper(input, input_start, input_end, output_start, output_end) {
 }
 
 function Getter() {
-    document.getElementById("demo").innerHTML = Mapper(
-        poles[poles.length - 1].x,
-        50,
-        550, -1,
-        1
-    );
-    PolesX.push(Mapper(poles[poles.length - 1].x, 50, 550, -1, 1));
-    PolesY.push(Mapper(poles[poles.length - 1].y, 50, 550, -1, 1));
+    const pointX = Mapper(poles[poles.length - 1].x, 50, 550, -1, 1);
+    const pointY = Mapper(poles[poles.length - 1].y, 50, 550, -1, 1);
+    document.getElementById("demo").innerHTML = pointX + " +j " + pointY;
+    PolesX.push(pointX);
+    PolesY.push(pointY);
     GraphData();
 }
 
 function GraphData() {
-    console.log(CircleNumbersX.length);
-    console.log(CircleNumbersY.length);
-    for (let i = 0; i < 315; i++) {
-        Distances.push(
-            Math.sqrt(
-                Math.pow(
-                    Mapper(poles[poles.length - 1].x, 50, 550, -1, 1) - CircleNumbersX[i],
-                    2
-                ) +
-                Math.pow(
-                    Mapper(poles[poles.length - 1].y, 50, 550, -1, 1) -
-                    CircleNumbersY[i],
-                    2
-                )
-            )
-        );
-    }
+    for (let j = 0; j < poles.length; j++) {
+        MuliOfDistances[j] = [];
+        MuliOfPhases[j] = [];
+        for (let i = 0; i < 315; i++) {
+            let pointX = Mapper(poles[j].x, 50, 550, -1, 1) - CircleNumbersX[i];
+            let pointY = Mapper(poles[j].y, 50, 550, -1, 1) - CircleNumbersY[i];
+            MuliOfDistances[j].push(
+                Math.sqrt(Math.pow(pointX, 2) + Math.pow(pointY, 2))
+            );
+            pointX = Mapper(poles[j].x, 50, 550, -1, 1) - CircleNumbersX[i];
+            pointY = Mapper(poles[j].y, 50, 550, -1, 1) - CircleNumbersY[i];
+            MuliOfPhases[j].push(
+                Math.atan(pointY / pointX)
+            );
+        }
+      }
+    
+      for (let j = 0; j < zeros.length; j++) {
+        DivsionOfDistances[j] = [];
+        MuliOfPhasesOfZeros[j] = [];
+        for (let i = 0; i < 315; i++) {
+            let pointX = Mapper(zeros[j].x, 50, 550, -1, 1) - CircleNumbersX[i];
+            let pointY = Mapper(zeros[j].y, 50, 550, -1, 1) - CircleNumbersY[i];
+            DivsionOfDistances[j].push(
+                Math.sqrt(Math.pow(pointX, 2) + Math.pow(pointY, 2))
+            );
+            pointX = Mapper(zeros[j].x, 50, 550, -1, 1) - CircleNumbersX[i];
+            pointY = Mapper(zeros[j].y, 50, 550, -1, 1) - CircleNumbersY[i];
+            MuliOfPhasesOfZeros[j].push(
+                Math.atan(pointY / pointX)
+            );
+        }
+      }
+    
+      var sum = [];
+      var sum2 = [];
+      var sum3 = [];
+      var sum4 = [];
+    
+      for (let K = 0; K < 315; K++) {
+        sum.push(1);
+        sum3.push(1);
+        sum2.push(0);
+        sum4.push(0);
+      }
+      for (let j = 0; j < poles.length; j++) {
+        Distances3 = [];
+        phases3 = [];
+        for (let i = 0; i < 315; i++) {
+          sum[i] = sum[i] * MuliOfDistances[j][i];
+          sum2[i] = sum2[i] + MuliOfPhases[j][i];
+    
+          Distances3.push(sum[i]);
+          phases3.push(sum2[i]);
+        }
+      }
 
-    for (let i = 0; i < 315; i++) {
-        console.log(Distances[i]);
-    }
-    console.log(Distances.length);
+      for (let j = 0; j < zeros.length; j++) {
+        Distances2 = [];
+        phases2 = [];
+        for (let i = 0; i < 315; i++) {
+          sum3[i] = sum3[i] * DivsionOfDistances[j][i];
+          sum4[i] = sum4[i] + MuliOfPhasesOfZeros[j][i];
+          Distances2.push(sum3[i]);
+          phases2.push(sum4[i]);
+        }
+      }
+      Distances = [];
+      phases = [];
+      for (let i = 0; i < 315; i++) {
+        if (zeros.length === 0) {
+          Distances.push(Distances3[i]);
+          phases.push(phases3[i]);
+        }
+        if (poles.length === 0) {
+          Distances.push(parseFloat(1 / Distances2[i]));
+          phases.push(phases2[i] * -1);
+        }
+        if (zeros.length !== 0 && poles.length !== 0) {
+          Distances.push(parseFloat(Distances3[i] / Distances2[i]));
+          phases.push(phases3[i] - phases2[i]);
+        }
+        }
 }
 
 function draw() {
@@ -159,3 +228,5 @@ class Zero extends Point {
         super(x, y, drawConjugate);
     }
 }
+
+// export {};
